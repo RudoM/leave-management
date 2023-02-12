6ea68df345ea64f233cb3e793d59b22e338ca135
RudoM <rudomlambo2@gmail.com>
2023-02-13 00:11:50 +0200

    Added apply and requests routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Livewire\Apply;
+use App\Http\Livewire\Requests;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +16,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return redirect('/apply');
 });
+
+Route::get('/apply', Apply::class);
+Route::get('/requests', Requests::class);
