5b55fe5848746f6f14485696831728c83b4c80b0
RudoM <rudomlambo2@gmail.com>
2023-02-13 01:41:35 +0200

    Implemented mount and added apply redirect

--- app/Http/Livewire/Requests.php
+++ app/Http/Livewire/Requests.php
@@ -2,12 +2,27 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Request;
 use Livewire\Component;
 
 class Requests extends Component
 {
+    public $requests;
+
+    public function mount()
+    {
+        $this->requests = Request::all();
+    }
+
+    public function apply()
+    {
+        return redirect('apply');
+    }
+    
     public function render()
     {
+
+
         return view('livewire.requests');
     }
 }
